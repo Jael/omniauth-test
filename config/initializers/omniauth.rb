b8df2fefe2506206343384cb0fd322ae6ed3184f
Jael <cutejael@gmail.com>
2012-10-05 13:32:58 +0800

    third

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, '7182ca3203492da3859d',
-          'c824773279b2c46f779c6e393c02935e19389ba9'
+          'c824773279b2c46f779c6e393c02935e19389ba9', scope: 'user'
 end
+
